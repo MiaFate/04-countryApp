b02ee88154088affbb384ec7861455160869a6ce
miafate <miapurplefroot@gmail.com>
2023-10-03 10:46:12 -0300

    feat persistence on bycountryPage

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
 
@@ -8,13 +8,22 @@ import { Country } from '../../interfaces/country.interface';
   styles: [
   ]
 })
-export class ByCountryPageComponent {
-  public countries: Country[] = []
+export class ByCountryPageComponent implements OnInit {
+  public isLoading: boolean = false;
+  public countries: Country[] = [];
+  public initialValue: string = "";
   constructor(private countriesService: CountriesService) { }
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountries.countries;
+    this.initialValue = this.countriesService.cacheStore.byCountries.term;
+
+  }
 
   searchByCountry(term: string): void {
+    this.isLoading = true
     this.countriesService.searchCountry(term).subscribe(countries => {
       this.countries = countries
+      this.isLoading = false
     })
   }
 }
