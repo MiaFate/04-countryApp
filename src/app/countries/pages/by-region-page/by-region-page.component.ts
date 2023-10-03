073a87af7fa2fba4e67494a2dc8b1278e1d08ba4
miafate <miapurplefroot@gmail.com>
2023-10-03 10:59:33 -0300

    feat persistence and loader on by-region-page

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
 import { Region } from '../../interfaces/region.type'
@@ -9,17 +9,24 @@ import { Region } from '../../interfaces/region.type'
   styles: [
   ]
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public countries: Country[] = [];
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   public selectedRegion?: Region;
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) { }
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
   searchByRegion(region: Region): void {
+    this.isLoading = true;
     this.selectedRegion = region;
     this.countriesService.searchRegion(region).subscribe(countries => {
-      this.countries = countries
+      this.countries = countries;
+      this.isLoading = false;
     })
   }
 
